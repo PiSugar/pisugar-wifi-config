ed053eb853ef98c6005ee46e3e949bcf258e7ec2
fengyingcai <fengyc.work@gmail.com>
2020-07-06 01:37:29 +0800

    update

--- README.md
+++ README.md
@@ -1,14 +1,30 @@
 # pisugar-wifi-config
-PiSugar BLE wifi config tool
+PiSugar BLE wifi config tool.
 
-## Installation
-
+## Requirements
 Install bluez
 
     sudo apt install bluez
 
-Enable dbus experimental features:
+Enable dbus experimental features (Required by old bluez):
 
     sudo sed -e 's|ExecStart=.*|ExecStart=/usr/lib/bluetooth/bluetoothd --experimental|g'
         -i /lib/systemd/system/bluetooth.service
 
+## Installation
+Packaging the project
+
+    python3 setup.py sdist
+
+Install
+
+    sudo pip3 install pisugar-wifi-config-<version>.tar.gz
+    sudo systemctl daemon-reload
+    sudo systemctl enable pisugar-wifi-config
+    sudo systemctl start pisugar-wifi-config
+
+## Bug report
+Report bugs here: https://github.com/PiSugar/pisugar-wifi-config/issues
+
+## License
+GPLv3
--- setup.py
+++ setup.py
@@ -1,24 +1,35 @@
-from setuptools import setup, find_packages
 
-setup(
-    name="pisugar-wifi-config",
-    version="0.1",
-    keywords = ("pisugar", "PiSugar", "wifi", "BLE"),
-    license = "GPLv3",
-    description = "PiSugar wifi BLE config tool",
-    long_description = "PiSugar wifi BLE config tool",
-    url = "https://www.pisugar.com/",
-    author = "PiSugar Team",
-    author_email = "pisugar.zero@gmail.com",
-    platforms = "any",
+import sys
+from setuptools import setup
 
-    packages = find_packages(),
-    include_package_data = True,
+VERSION="0.1"
 
-    scripts = ["pisugar_wifi_config.py"],
-    entry_points={
-        "console_scripts": [
-            "pisugar-wifi-config = pisugar_wifi_config:main",
-        ]
-    }
-)
+def main():
+    setup(
+        name="pisugar-wifi-config",
+        version=VERSION,
+        license = "GPLv3",
+        keywords = ["pisugar", "PiSugar", "wifi", "BLE"],
+        description = "PiSugar wifi BLE config tool",
+        long_description = ''.join([l for l in _get_long_description()]),
+        url = "https://www.pisugar.com/",
+        author = "PiSugar Team",
+        author_email = "pisugar.zero@gmail.com",
+        platforms = ["all"],
+        packages = ['pisugar_wifi_config'],
+
+        package_data = {"":["*.service"]},
+        data_files = [("/lib/systemd/system", ["pisugar-wifi-config.service"])],
+        entry_points={
+            'console_scripts': [
+                'pisugar-wifi-config=pisugar_wifi_config:main',
+            ],
+        }
+    )
+
+def _get_long_description(fname='README.md'):
+    for line in open(fname, 'r'):
+        yield line
+
+if __name__ == '__main__':
+    main()
